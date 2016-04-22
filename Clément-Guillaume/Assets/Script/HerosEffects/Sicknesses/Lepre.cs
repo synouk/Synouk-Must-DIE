66514795b64670a08fc09ff0c8602e21bdaba99b
Anthaax <guillaume.fimes@laposte.net>
2016-04-22 18:23:22 +0200

    Ajouts des changement fait par Clément

---
+++ Clément-Guillaume/Assets/Script/HerosEffects/Sicknesses/Lepre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class Lepre : BaseSickness
+{
+    public Lepre()
+    {
+        string name = "Lepre";
+        int priceToRemove = 850;
+        string description = "Evitez la guitare!";
+    }
+
+    public override void affectToHeros(BaseHerosClass heros)
+    {
+        heros.Damage = Convert.ToInt32(heros.Damage * 0.5);
+    }
+
+    public override void cancelAffectToHeros(BaseHerosClass heros)
+    {
+        heros.Damage = Convert.ToInt32(heros.Damage * 2);
+    }
+}
